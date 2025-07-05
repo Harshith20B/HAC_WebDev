2475e5571587de73a89ed6ece8e9b514a70e306c
Harshith Bezawada <>
2025-07-05 09:19:48 +0530

    deploy

--- backend/build.sh
+++ backend/build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-npm install
+echo "Installing pyenv..."
+curl https://pyenv.run | bash
 
-echo "Installing Python dependencies..."
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+pyenv install 3.11.8
+pyenv global 3.11.8
+
+echo "Python version:"
+python --version
+
 pip install --upgrade pip setuptools wheel
 pip install -r ./python/requirements.txt
+
+npm install
